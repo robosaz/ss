01be191942724c8b9eb076ca7fbc0d9962d7e52e
roboxt <robo.xt7aj@gmail.com>
2017-01-10 22:38:22 +0330

    Update sazande.lua

--- plugins/sazande.lua
+++ plugins/sazande.lua
@@ -1,10 +1,9 @@
 local function run(msg, matches)
-   if matches == 'صاحب' and is_sudo(msg) then
     return [[ شاهین رضایی تبریزی ]]
 end return 
 {
     patterns = {
-    "^(صاحب)$"
+    "^صاحب$"
   },
   run = run
 }
