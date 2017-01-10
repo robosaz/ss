c71ccb7804fc116f37139080f32d547321e0b8db
roboxt <robo.xt7aj@gmail.com>
2017-01-10 22:57:43 +0330

    Update s2.lua

--- plugins/s2.lua
+++ plugins/s2.lua
@@ -1,8 +1,6 @@
 local function run(msg, matches)
      if matches == 'بات' and is_sudo(msg) then    
-      return [[ بله مدیر چه کمکی از دستم برمیاد انجام بدم؟ ]]
- 
-        end 
+      return [[ بله مدیر چه کمکی از دستم برمیاد انجام بدم؟ ]]       
    return 
 {
     patterns = {
