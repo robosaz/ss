f15a3df51fbfd2b0702d313c0a87dca286a6ad17
roboxt <robo.xt7aj@gmail.com>
2017-01-10 23:36:11 +0330

    Update s2.lua

--- plugins/s2.lua
+++ plugins/s2.lua
@@ -1,5 +1,8 @@
 local function run(msg, matches)
-     if matches[1] == 'ای' and is_sudo(msg) then   
+     if matches[1] == 'ای' then
+          if is_sudo(msg) then   
+       end
+     end
      return "بله"
   end
 return {         
