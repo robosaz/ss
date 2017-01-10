bb58dadfb91db66738982d3c849483c2923dc996
roboxt <robo.xt7aj@gmail.com>
2017-01-10 22:31:59 +0330

    Update sud.lua

--- plugins/sud.lua
+++ plugins/sud.lua
@@ -1,11 +1,9 @@
 local function run(msg, matches)
-    if matches[1] == 'ربات' and is_sudo(msg) then
-     if msg.to == 'channel' then
       return [[ بله مدیر چه کمکی از دستم برمیاد انجام بدم؟ ]]
 end return 
 {
     patterns = {
-    "^(ربات)$"
+    "^بات$"
   },
   run = run
 }
